87ae2fc778634b0e3b6363df799259f5224c4d10
screenleon <screen.leon@gmail.com>
2020-09-17 09:38:32 +0800

    Fix resource not found, Cookie update with website language

--- intro/js/renderLanguage.js
+++ intro/js/renderLanguage.js
@@ -1,8 +1,14 @@
-function renderLanguage(){
-    if(!!document.cookie) return;
+function renderLanguage() {
     const supportLanguages = ['zh-TW', 'zh'];
-    navigator.languages.forEach(function(element) {
-        if(supportLanguages.indexOf(element) !== -1){
+    if (!!document.cookie) {
+        const cookieLanguage = /language=([a-z]{1,8}(-[a-z]{1,8})?)/i.exec(document.cookie)[1];
+        if(cookieLanguage !== navigator.language){
+            document.cookie = `language=${navigator.language}`;
+        }
+        return;
+    }
+    navigator.languages.forEach(function (element) {
+        if (supportLanguages.indexOf(element) !== -1) {
             document.cookie = "language=zh-TW";
             window.location.assign('https://screenleon.github.io/index_zh-TW.html');
             return;
