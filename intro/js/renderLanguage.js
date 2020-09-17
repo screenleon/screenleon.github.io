29fa22da55fdaa7f361e035f4437c53f103f1eba
screenleon <screen.leon@gmail.com>
2020-09-17 10:14:48 +0800

    Fixed set the wrong cookie, Adjust render page

--- intro/js/renderLanguage.js
+++ intro/js/renderLanguage.js
@@ -2,18 +2,18 @@ function renderLanguage() {
     const supportLanguages = ['zh-TW', 'zh'];
     if (!!document.cookie) {
         const cookieLanguage = /language=([a-z]{1,8}(-[a-z]{1,8})?)/i.exec(document.cookie)[1];
-        if(cookieLanguage !== navigator.language){
-            document.cookie = `language=${navigator.language}`;
+        if(cookieLanguage !== document.documentElement.lang){
+            document.cookie = `language=${document.documentElement.lang};`;
         }
         return;
     }
     navigator.languages.forEach(function (element) {
         if (supportLanguages.indexOf(element) !== -1) {
-            document.cookie = "language=zh-TW";
-            window.location.assign('https://screenleon.github.io/index_zh-TW.html');
+            document.cookie = "language=zh-TW;";
+            window.location.assign('./index_zh-TW.html');
             return;
         }
     })
-    document.cookie = "language=en";
+    document.cookie = "language=en;";
     return;
 }
